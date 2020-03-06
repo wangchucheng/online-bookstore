e0b6c0185826e0068a60490834a15c9b9755d235
Wang Chucheng <ccwangchn@gmail.com>
2020-03-06 11:41:09 +0800

    Initial commit

---
+++ src/test/java/com/wangchucheng/onlinebookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangchucheng.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
